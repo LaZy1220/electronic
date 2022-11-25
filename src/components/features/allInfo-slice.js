4173756cfc8379f1f4757ae795f516e92d9b039b
LaZy1220 <4elove4iska@gmail.com>
2022-11-25 16:35:27 +0300

    add mainContent

---
+++ src/components/features/allInfo-slice.js
@@ -0,0 +1,32 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getInfoPage = createAsyncThunk("@@allInfo/load-info", async () => {
+  return axios.get("http://electrical.makser-test.site/api/all/");
+});
+const initialState = {
+  status: "idle",
+  list: [],
+  error: null,
+};
+const allInfoSlice = createSlice({
+  name: "@@allInfo",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getInfoPage.pending, (state) => {
+        state.status = "loading";
+        state.error = "null";
+      })
+      .addCase(getInfoPage.rejected, (state, action) => {
+        state.status = "rejected";
+        state.error = action.payload || action.meta.error;
+      })
+      .addCase(getInfoPage.fulfilled, (state, action) => {
+        state.status = "received";
+        state.list = action.payload.data.AllValues;
+      });
+  },
+});
+export const allInfoReducer = allInfoSlice.reducer;
