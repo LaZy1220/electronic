1570e953a74ae5b0a400aab5a55dd6d7ad02232d
LaZy1220 <4elove4iska@gmail.com>
2022-12-05 15:45:17 +0300

    Finish services

--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -1,15 +1,29 @@
 import styled from "styled-components";
+import Arrows from "../../images/arrowDown.png";
 
 const ServiceEl = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 20px;
-  height: 98px;
+  height: ${({ isActive }) => (isActive ? "auto" : "98px")};
   position: relative;
   overflow: hidden;
+  @media screen and (max-width: 1120px) {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding-bottom: 20px;
+  }
 `;
 const Text = styled.p`
   width: 400px;
+  @media screen and (max-width: 1350px) {
+    width: 70%;
+  }
+  @media screen and (max-width: 1200px) {
+    width: 70%;
+    font-size: 16px;
+  }
 `;
 
 const Type = styled.div`
@@ -19,23 +33,63 @@ const Type = styled.div`
   p ~ p {
     text-decoration: underline;
   }
+  @media screen and (max-width: 1120px) {
+    min-width: 100%;
+  }
 `;
 const Button = styled.button`
-  position: absolute;
-  right: 180px;
+  position: relative;
+  left: ${({ isActive }) => (isActive ? "95%" : "91%")};
+  top: 10px;
+  border: none;
+  cursor: pointer;
+  @media screen and (max-width: 900px) {
+    left: ${({ isActive }) => (isActive ? "90%" : "85%")};
+  }
+  @media screen and (max-width: 620px) {
+    left: ${({ isActive }) => (isActive ? "90%" : "80%")};
+  }
+  @media screen and (max-width: 500px) {
+    left: ${({ isActive }) => (isActive ? "85%" : "75%")};
+  }
+  @media screen and (max-width: 420px) {
+    left: ${({ isActive }) => (isActive ? "75%" : "65%")};
+  }
 `;
-export const Service = ({ service }) => {
+export const Service = ({ service, isActive, handleActive }) => {
   return (
     <>
-      <ServiceEl>
+      <ServiceEl isActive={isActive}>
         {service?.map((item) => (
-          <Type>
+          <Type key={item.id}>
             <Text>{item.service}</Text>
             <p>от {item.price} руб.</p>
           </Type>
         ))}
       </ServiceEl>
-      <Button>Читать больше </Button>
+      {isActive ? (
+        <Button onClick={() => handleActive()} isActive={isActive}>
+          Скрыть
+          <img
+            style={{
+              transform: "rotate(180deg)",
+              height: "10px",
+              paddingRight: "5px",
+            }}
+            src={Arrows}
+            alt="arrow"
+          />
+        </Button>
+      ) : (
+        <Button onClick={() => handleActive()} isActive={isActive}>
+          Читать больше
+          <img
+            style={{ paddingLeft: "5px", height: "10px" }}
+            src={Arrows}
+            alt="img"
+          />
+        </Button>
+      )}
     </>
   );
 };
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,32 +2,66 @@ import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { Title } from "../Title";
 import { Service } from "./Service";
+import { useState } from "react";
 
 const ServicesEl = styled.div`
   margin: auto 180px;
   margin-top: 110px;
+  @media screen and (max-width: 1120px) {
+    margin-left: 5%;
+    margin-right: 5%;
+  }
 `;
 const SubTitle = styled.h4`
   text-decoration: underline;
   font-size: 40px;
   font-weight: 400;
   margin-bottom: 80px;
-  margin-top: 40px;
+  padding-top: 50px;
+  @media screen and (max-width: 1120px) {
+    font-size: 30px;
+    margin-top: 0;
+    margin-bottom: 30px;
+  }
 `;
 
 export const Services = () => {
   const draftService = useSelector((state) => state.allInfo.list.draft_service);
   const cleanService = useSelector((state) => state.allInfo.list.clean_service);
   const legalService = useSelector((state) => state.allInfo.list.legal_service);
+  const [isActive1, setActive1] = useState(false);
+  const [isActive2, setActive2] = useState(false);
+  const [isActive3, setActive3] = useState(false);
+  const handleActive1 = () => {
+    setActive1(!isActive1);
+  };
+  const handleActive2 = () => {
+    setActive2(!isActive2);
+  };
+  const handleActive3 = () => {
+    setActive3(!isActive3);
+  };
   return (
-    <ServicesEl>
+    <ServicesEl id="price">
       <Title>Прайс</Title>
       <SubTitle>Черновая электрика</SubTitle>
-      <Service service={draftService} />
+      <Service
+        isActive={isActive1}
+        handleActive={handleActive1}
+        service={draftService}
+      />
       <SubTitle>Чистовая электрика</SubTitle>
-      <Service service={cleanService} />
+      <Service
+        isActive={isActive2}
+        handleActive={handleActive2}
+        service={cleanService}
+      />
       <SubTitle>Виды услуг для юридических лиц</SubTitle>
-      <Service service={legalService} />
+      <Service
+        isActive={isActive3}
+        handleActive={handleActive3}
+        service={legalService}
+      />
     </ServicesEl>
   );
 };
